0148512e8dc92d8f9b1cead21b07a00e3bed701b
Virendra Sahu <virendra2609.vs@gmail.com>
2025-07-22 15:36:14 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,8 +13,8 @@ const Index = () => {
       <Navigation />
       <Hero />
       <About />
-      <Skills />
       <Education />
+      <Skills />
       <Projects />
       <Contact />
       <Footer />
